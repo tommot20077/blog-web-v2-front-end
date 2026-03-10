d62c126294ca76202a4144bd5983a97dc2b5bb6e
ablckone16 <herouknow830116@gmail.com>
2026-03-10 15:29:50 +0800

    新增 src 以及 HomeView、Navbar、Footer 組件

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const navItems = [
+    { name: '首頁', path: '/' },
+    { name: '文章', path: '/articles' },
+    { name: '登入', path: '/login'},
+]
+</script>
+<template>
+    <nav>
+        <RouterLink 
+            v-for="item in navItems" 
+            :to="item.path" 
+            :key="item.path"
+        >
+            {{item.name}}
+        </RouterLink>
+    </nav>
+</template>
